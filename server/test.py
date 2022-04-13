f051fa20f92a1171393a34f15001fab254a70847
Naman Jain <jnamanjain2001@gmail.com>
2022-04-13 21:51:35 +0530

    fixed file location in test.py

--- server/test.py
+++ server/test.py
@@ -1,10 +1,10 @@
 import requests
 
-images = ['../driver/data/test/Closed/_102.jpg', '../driver/data/test/Open/_117.jpg',
-          '../driver/data/test/yawn/118.jpg', '../driver/data/test/no_yawn/1042.jpg',
-          '../driver/data/test/no_yawn/1008.jpg', '../driver/data/test/yawn/79.jpg',
-          '../driver/data/test/no_yawn/833.jpg', '../driver/data/test/no_yawn/1202.jpg',
-          '../driver/data/val/yawn/11.jpg', '../driver/data/val/yawn/532.jpg']
+images = ['../pytorch/data/test/Closed/_102.jpg', '../pytorch/data/test/Open/_117.jpg',
+          '../pytorch/data/test/yawn/118.jpg', '../pytorch/data/test/no_yawn/1042.jpg',
+          '../pytorch/data/test/no_yawn/1008.jpg', '../pytorch/data/test/yawn/79.jpg',
+          '../pytorch/data/test/no_yawn/833.jpg', '../pytorch/data/test/no_yawn/1202.jpg',
+          '../pytorch/data/val/yawn/11.jpg', '../pytorch/data/val/yawn/532.jpg']
 
 for image in images:
     resp = requests.post("http://localhost:5000/predict",
